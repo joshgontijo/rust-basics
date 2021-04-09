bda1c7c6cfb87d56b0eb2f75052716bf74d77c25
Josh Gontijo <josh.gontijo@oracle.com>
2021-04-09 14:59:21 +0100

    Added and_then to threadpool

--- threadpool/src/main.rs
+++ threadpool/src/main.rs
@@ -2,16 +2,20 @@ use futures::executor::{ThreadPool, block_on};
 use futures::task::SpawnExt;
 use std::time::Duration;
 use std::thread::sleep;
+use futures::TryFutureExt;
+use std::sync::mpsc;
+use std::sync::atomic::{AtomicIsize, AtomicUsize, AtomicU32};
 
 fn main() -> Result<(), std::io::Error> {
     let pool = ThreadPool::builder()
         .name_prefix("my-pool-")
         .create()?;
 
-    let result = pool.spawn_with_handle(do_async()).unwrap();
+    let future = do_async().and_then(add10);
+    let result = pool.spawn_with_handle(future).unwrap();
     let computed = block_on(result);
 
-    println!("RESULT: {}", computed);
+    println!("RESULT: {:?}", computed);
     Ok(())
 }
 
@@ -21,9 +25,15 @@ let future = async {
     ....
 }
 **/
-async fn do_async() -> i32 {
+async fn do_async() -> Result<i32, ()> {
     sleep(Duration::from_secs(5));
-    if let Some(name) = std::thread::current().name() { println!("DONE {}", name) }
-    1
+    if let Some(name) = std::thread::current().name() { println!("do_async {}", name) }
+    Ok(1)
+}
+
+async fn add10(x: i32) -> Result<i32, ()> {
+    sleep(Duration::from_secs(5));
+    if let Some(name) = std::thread::current().name() { println!("add10 {}", name) }
+    Ok(x + 10)
 }
 
