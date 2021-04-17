b5458562f4cfc1a34c29e28418b4b8e7f18393b8
Josh <josue.eduardo206@gmail.com>
2021-04-17 21:04:10 +0100

    Added some more closure code

--- closure/src/main.rs
+++ closure/src/main.rs
@@ -14,6 +14,16 @@ fn main() {
     let mut map = HashMap::<i32, String>::new();
     map.entry(1).or_insert_with(|| "".to_string());
 
+
+    //storing function pointer of a struct into another one
+    let some_struct = SomeStruct {value : 123};
+    let with_pointer = WithFuncPointerToAnotherStruct{ fun: SomeStruct::do_something };
+
+    //calls function in a field of a struct
+    let res = (with_pointer.fun)(&some_struct);
+    println!("{}", res)
+
+
 }
 
 fn apply_fn(user: &User, fun: &dyn Fn(&User) -> u32) -> u32 {
@@ -25,7 +35,20 @@ fn returns_closure() -> Box<dyn Fn(&User) -> u32> {
 }
 
 
-
 struct User {
     age: u32
+}
+
+struct WithFuncPointerToAnotherStruct {
+    fun: fn(&SomeStruct) -> u32,
+}
+
+struct SomeStruct {
+    value: u32
+}
+
+impl SomeStruct {
+    fn do_something(&self) -> u32 {
+        self.value
+    }
 }
