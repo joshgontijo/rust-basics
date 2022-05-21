6463b1ced992343b5d31be59a47ec0abd314d422
josueeduardo <josue.egontijo@gmail.com>
2022-05-21 02:05:36 +0100

    Refactoring

---
+++ ecs/src/component.rs
@@ -0,0 +1,157 @@
+use std::any::{Any, TypeId};
+use std::cell::{Ref, RefCell, RefMut};
+use std::collections::HashMap;
+use std::fmt::{Debug, Formatter};
+use std::marker::PhantomData;
+use std::rc::Rc;
+
+use crate::{EntityId, World};
+
+#[derive(Default)]
+pub struct Components {
+    items: HashMap<TypeId, Vec<Option<Rc<RefCell<dyn Any>>>>>,
+}
+
+pub struct Component<T: Any> {
+    inner: Rc<RefCell<dyn Any>>,
+    _m: PhantomData<T>,
+}
+
+
+impl Components {
+    pub(crate) fn new(items: HashMap<TypeId, Vec<Option<Rc<RefCell<dyn Any>>>>>) -> Self {
+        Self { items }
+    }
+
+    pub fn new_entity(&mut self) {
+        self.items.values_mut().for_each(|components| components.push(None));
+    }
+
+    pub fn get_component<T: Any>(&self, entity_id: EntityId) -> Option<Component<T>> {
+        self.items.get(&TypeId::of::<T>())
+            .expect("Component not registered")
+            .get(entity_id)
+            .map(|e| {
+                match e {
+                    None => None,
+                    Some(t) => Some(Component::new(Rc::clone(t)))
+                }
+            }).flatten()
+    }
+
+    pub fn add_component<T: Any>(&mut self, entity_id: EntityId, component: T) {
+        let component_vec = self.items.get_mut(&TypeId::of::<T>()).expect("Component type not registered");
+        component_vec.insert(entity_id, Some(Rc::new(RefCell::new(component))));
+    }
+
+    pub(crate) fn query<Tuple>(&self) -> ComponentsIter<Tuple> {
+        ComponentsIter {
+            entity_idx: 0,
+            components: self,
+            _m: PhantomData,
+        }
+    }
+}
+
+
+pub struct ComponentsIter<'a, Tuple> {
+    entity_idx: usize,
+    components: &'a Components,
+    _m: PhantomData<Tuple>,
+}
+
+impl<'a, Tuple: Fetch> Iterator for ComponentsIter<'a, Tuple> {
+    type Item = Tuple::Data;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let components = &mut self.components;
+        let res = Tuple::fetch(components, self.entity_idx);
+        self.entity_idx += 1;
+        res
+    }
+}
+
+
+impl<T: Any + Debug> Debug for Component<T> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        self.as_ref().fmt(f)
+    }
+}
+
+impl<T: Any> Component<T> {
+    fn new(rc: Rc<RefCell<dyn Any>>) -> Self {
+        Self {
+            inner: rc,
+            _m: PhantomData,
+        }
+    }
+
+    fn inner_type(&self) -> TypeId {
+        TypeId::of::<T>()
+    }
+    // fn as_ref(& self) -> impl Deref<Target = T> + '_ {
+    //     Ref::map(self.inner.borrow(), |any| any.downcast_ref::<T>().unwrap())
+    // }
+
+    pub fn as_ref(&self) -> Ref<'_, T> {
+        Ref::map(self.inner.borrow(), |any| any.downcast_ref::<T>().unwrap())
+    }
+
+    pub fn as_ref_mut(&self) -> RefMut<'_, T> {
+        RefMut::map(self.inner.borrow_mut(), |any| any.downcast_mut::<T>().unwrap())
+    }
+}
+
+
+pub trait Fetch {
+    type Data;
+    fn fetch(components: &Components, idx: usize) -> Option<Self::Data>;
+}
+
+/// Resolves to:
+/// impl<T1, T2> Fetch for (T1, T2)
+///    where
+///       T1: Any,
+///       T2: Any
+///  {
+///      type Data = (Component<T1>, Component<T2>);
+///
+///      fn fetch(world: &World, idx: usize) -> Option<Self::Data> {
+///          Some((world.get_component::<T1>(idx)?, world.get_component::<T2>(idx)?))
+///      }
+///  }
+macro_rules! fetch_tuple {
+
+     ($($ty: ident),*) => {// match like arm for macro
+          impl<$($ty,)*> Fetch for ($($ty,)*)
+            where
+                $(
+                    $ty: Any,
+                )*
+
+         {
+            type Data = ($(Component<$ty>,)*);
+
+            fn fetch(components: &Components, idx: usize) -> Option<Self::Data> {
+                // let t1 = world.get::<T1>(idx);
+                // let t2 = world.get::<T2>(idx);
+                // let res = ( world.get::<T1>(idx)?, world.get::<T2>(idx)?);
+                // return Some(res);
+
+                Some(($(components.get_component::<$ty>(idx)?,)*))
+                }
+         }
+    }
+}
+
+
+fetch_tuple! {T0}
+fetch_tuple! {T0, T1}
+fetch_tuple! {T0, T1, T2}
+fetch_tuple! {T0, T1, T2, T3}
+fetch_tuple! {T0, T1, T2, T3, T4}
+fetch_tuple! {T0, T1, T2, T3, T4, T5}
+fetch_tuple! {T0, T1, T2, T3, T4, T5, T6}
+fetch_tuple! {T0, T1, T2, T3, T4, T5, T6, T7}
+fetch_tuple! {T0, T1, T2, T3, T4, T5, T6, T7, T8}
+fetch_tuple! {T0, T1, T2, T3, T4, T5, T6, T7, T8, T9}
--- ecs/src/system.rs
+++ ecs/src/system.rs
@@ -1,2 +1,55 @@
+use std::marker::PhantomData;
+use std::slice::{Iter, IterMut};
+use crate::{Components, Fetch};
 
+struct System<T> where T: Fetch {
+    f: fn(T::Data),
+    _m: PhantomData<T>,
+}
+
+impl<T> System<T> where T: Fetch {
+    fn new(f: fn(T::Data)) -> Self {
+        Self {
+            f,
+            _m: PhantomData,
+        }
+    }
+
+
+
+}
+
+#[derive(Default)]
+pub struct Systems {
+    items: Vec<Box<dyn SystemRunner + 'static>>,
+}
+
+impl Systems {
+    pub fn add_system<T>(&mut self, f: fn(T::Data))
+        where
+            T: Fetch + 'static
+    {
+        self.items.push(Box::new(System::<T>::new(f)));
+    }
+
+    pub fn iter_mut(&mut self) -> IterMut<'_, Box<dyn SystemRunner>> {
+        self.items.iter_mut()
+    }
+
+}
+
+pub trait SystemRunner {
+    fn run(&mut self, components: &Components);
+}
+
+
+
+impl<T: Fetch> SystemRunner for System<T> {
+    fn run(&mut self, components: &Components) {
+        let mut iter = components.query::<T>();
+        while let Some(item) = iter.next() {
+            (self.f)(item);
+        }
+    }
+}
 
