0951fdbdcd1921dc9d0feb2f15c4eecc32098082
Josh Gontijo <josue.egontijo@gmail.com>
2022-06-06 14:33:12 +0100

    Tidy up

--- scratch/src/main.rs
+++ scratch/src/main.rs
@@ -7,7 +7,7 @@ mod component;
 
 fn main() {
     let mut items = HashMap::<TypeId, Vec<Option<Box<dyn Any>>>>::new();
-    items.insert(TypeId::of::<u32>(), vec![Some(Box::new(123))]);
+    items.insert(TypeId::of::<u32>(), vec![Some(Box::new(123u32))]);
     items.insert(TypeId::of::<String>(), vec![Some(Box::new("ABC".to_string()))]);
 
     let mut components = Components::new(items);
